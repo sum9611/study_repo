c51eda8ac652619c0f964e2d23c02a2c26a25841
sum9611 <77780130+sum9611@users.noreply.github.com>
2023-02-23 18:25:21 +0900

    [Silver IV] Title: 수 찾기, Time: 492 ms, Memory: 46740 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,31 @@
+from sys import stdin
+
+# input = stdin.readline
+n = int(input())
+a_list = list(map(int,input().split()))
+m = int(input())
+m_list = list(map(int,input().split()))
+
+a_list.sort()
+
+def check(n_list, i, start, end):
+    
+    # 해당값이 없을경우 탈출하는 로직 
+    if start > end :
+        return print(0, end = ' ')
+    center = (start + end) //2
+    
+    # 값을 찾은경우     
+    if n_list[center] == i:
+        return print(1, end = ' ')
+
+    # i 값이 중간값보다 작은경우 end 값 변경
+    elif n_list[center] > i:
+        return check(n_list, i, start, center - 1)
+
+    # i 값이 중간값보다 큰 경우 start 값 변경 
+    else:
+        return check(n_list, i , center +1 , end)    
+
+for i in m_list:
+    check(a_list,i,0,n-1)
