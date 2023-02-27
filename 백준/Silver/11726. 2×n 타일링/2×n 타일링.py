6bcc3164fe934f34c1555f651b89a37b5171d99e
sum9611 <77780130+sum9611@users.noreply.github.com>
2023-02-27 16:42:02 +0900

    [Silver III] Title: 2×n 타일링, Time: 80 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+# input = stdin.readline
+
+dp = [0] * 1001
+
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3,1001):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[int(input())]%10007)
