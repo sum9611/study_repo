1903f5b382b967192a0d6282cd686a1ff72641f4
sum9611 <77780130+sum9611@users.noreply.github.com>
2023-02-27 21:05:48 +0900

    [Gold V] Title: LCS, Time: 508 ms, Memory: 55712 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,16 @@
+S1 = list(input())
+S2 = list(input())
+len1 = len(S1)
+len2 = len(S2)
+dp = [[0]*(len2 + 1) for _ in range(len1+1)]
+
+
+for i in range(1,len1 + 1) :
+    for j in range(1,len2 + 1) :
+        if S1[i-1] == S2[j-1] :
+            dp[i][j] = dp[i-1][j-1] + 1
+        else :
+            dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+
+
+print(dp[len1][len2])
