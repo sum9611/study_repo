fcfbfc47242b020eba8bdebc618beda171355162
sum9611 <77780130+sum9611@users.noreply.github.com>
2023-03-14 18:21:13 +0900

    [Silver III] Title: 1로 만들기, Time: 600 ms, Memory: 39068 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,22 +1,28 @@
 from sys import stdin
 
-input = stdin.readline
+# input = stdin.readline
 
-n = int(input().rstrip())
+n = int(input())
+dp = [0] * (n+1)
 
-dp = [0] * (n+3)
-
-dp[1] = 0
-dp[2] = 1
-dp[3] = 1
-if n <4:
-    print(dp[n])
-
+if n == 1:
+    print(0)
+elif n == 2:
+    print(1)
+elif n == 3:
+    print(1)
 else:
+    dp[1] = 0
+    dp[2] = 1
+    dp[3] = 1
+
     for i in range(4,n+1):
-        dp[i] = dp[int(i-1)] +1
-        if i%2 == 0:
-            dp[i] = min(dp[int(i/2)] +1, dp[i])
-        if i%3 == 0:
-            dp[i] = min(dp[int(i/3)] +1, dp[i])
+        if i%2 == 0 and i%3 == 0:
+            dp[i] = min(dp[int(i/2)] + 1, dp[i-1] + 1, dp[int(i/3)] + 1)
+        elif i%2 == 0:
+            dp[i] = min(dp[int(i/2)] + 1, dp[i-1] + 1)
+        elif i%3 == 0:
+            dp[i] = min(dp[int(i/3)] + 1, dp[i-1] + 1)
+        else:
+            dp[i] = dp[i-1] + 1
     print(dp[n])
